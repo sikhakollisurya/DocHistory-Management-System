35d7e558c58d0446e146d1b140a043d4e25c1ecf
Sikhakolli Surya <133906322+sikhakollisurya@users.noreply.github.com>
2024-04-18 22:12:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask,send_from_directory,render_template
+from flask import send_from_directory
+
+from flask_restful import Resource, Api
+from package.patient import Patients, Patient
+from package.doctor import Doctors, Doctor
+from package.appointment import Appointments, Appointment
+from package.common import Common
+from package.department import Departments, Department
+
+import json
+import os
+
+with open('config.json') as data_file:
+    config = json.load(data_file)
+
+app = Flask(__name__, static_url_path='')
+api = Api(app)
+
+api.add_resource(Patients, '/patient')
+api.add_resource(Patient, '/patient/<int:id>')
+api.add_resource(Doctors, '/doctor')
+api.add_resource(Doctor, '/doctor/<int:id>')
+api.add_resource(Appointments, '/appointment')
+api.add_resource(Appointment, '/appointment/<int:id>')
+api.add_resource(Common, '/common')
+
+api.add_resource(Departments, '/department')
+api.add_resource(Department, '/department/<int:department_id>')
+
+# Routes
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host=config['host'],port=config['port'])
